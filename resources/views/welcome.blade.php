0d8dbce36d009f9ff688637aa36d4ddb7298e617
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-12-24 10:00:39 +0700

    Laravel Livewire 01

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @livewireStyles
+</head>
+<body>
+    <h1>Demo Livewire</h1>
+    <livewire:counter />
+    @livewireScripts
+</body>
+</html>
