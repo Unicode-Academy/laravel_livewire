2f7886097ba456c82fb0e3128d4660352ac23a01
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-01-01 09:01:29 +0700

    Buoi 33

--- app/Livewire/Toggle.php
+++ app/Livewire/Toggle.php
@@ -3,14 +3,26 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Toggle extends Component
 {
-    public $isShow = false;
+    public $isShow = true;
+    public $text = null;
 
     public function handleReset()
     {
         $this->isShow = true;
+    }
+    #[On('toggle-clicked')]
+    public function handleClick($text, $number)
+    {
+        dd($text, $number);
+    }
+
+    public function handleSearch($name)
+    {
+        dd($name);
     }
 
     public function render()
--- resources/views/livewire/toggle.blade.php
+++ resources/views/livewire/toggle.blade.php
@@ -1,17 +1,15 @@
-<div class="container p-3" x-data="{ isShow: @js($isShow) }">
-    {{--
-    @if ($isShow)
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quod id at. Voluptas repellat, est architecto
-        numquam fuga laborum vitae distinctio molestias dolores praesentium! Cumque odio eum commodi nihil nam!</p>
-    @endif
-   
-        <button class="btn btn-primary" wire:click="$toggle('isShow')">{{$isShow ? 'Ẩn':'Hiện'}}</button>
-    --}}
+<div class="container p-3" x-data="{ isShow: @js($isShow), name: '' }">
     <p x-show="isShow">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod officiis sapiente, non, asperiores ipsa eum
         laborum praesentium suscipit esse odio facere laboriosam iusto ullam deleniti, autem eaque rerum voluptates?
         Architecto.
     </p>
-    <button x-on:click="isShow = !isShow">Toggle</button>
-    <livewire:content />
+    <div>
+        <input type="text" x-on:keyup="$wire.handleSearch(event.target.value)" />
+        <h3 x-text="name"></h3>
+    </div>
+    <div>
+        <button x-on:click="isShow = !isShow">Toggle</button>
+        <button x-on:click="$wire.$dispatch('toggle-clicked', ['Unicode Academy', 10])">Click me</button>
+    </div>
 </div>
